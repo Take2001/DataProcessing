4e7bb63789c1b1df09d50512c5d652190d7f2066
Bob Leijnse <bobleynse@hotmail.com>
2018-06-14 11:41:33 +0200

    added a function to import de datasets from every function

---
+++ helpfiles/select_df.py
@@ -0,0 +1,49 @@
+
+# coding: utf-8
+
+# In[86]:
+
+
+#aantal mappen terug naar DataProcessing #de dataset die je wil importeren 
+
+def select_df(folders_up, dataset):
+    import os
+    import pandas as pd
+    import numpy as np
+    
+    print(os.getcwd())
+    
+    if dataset == 'foodprices':
+        for i in range(folders_up):
+            os.chdir("../")
+        os.chdir("datasets/preprocessed datasets/Food prices")
+        raw_data = pd.read_csv('Food_prizes_Africa_processed.csv')
+        df = pd.DataFrame(raw_data)
+        print('imported dataset as: df_foodprices')   
+        
+    elif dataset == 'migration_movements':
+        for i in range(folders_up):
+            os.chdir("../")                   
+        os.chdir("datasets/preprocessed datasets/Migration movements")
+        raw_data = pd.read_csv('data_africa.csv')
+        df = pd.DataFrame(raw_data)
+        print('imported dataset as: df_migration_movements')    
+                                    
+    elif dataset == 'temperature_and_precipitation':
+        for i in range(folders_up):
+            os.chdir("../")        
+        os.chdir("datasets/preprocessed datasets/Temperature and precipitation")
+        raw_data = pd.read_csv('tas_pr_1991_2015_AC.csv')
+        df = pd.DataFrame(raw_data)
+        print('imported dataset as: df_temperature_and_precipitation')
+    else:
+        return('Error: -- {} -- is geen beschikbare dataset. Kies uit: foodprices, migration_movements of temperature_and_precipitation'.format(dataset))
+    
+    if folders_up < 3:    
+        for i in range(3 - folders_up ):
+            os.chdir("../")
+
+    print(os.getcwd())
+    return df
+select_df(2, 'hoi')
+
