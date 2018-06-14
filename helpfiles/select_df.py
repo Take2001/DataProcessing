5ebfab181c98c041e162b4c72aad81fcc02748d6
Bob Leijnse <bobleynse@hotmail.com>
2018-06-14 12:28:54 +0200

    simplified the selective plot function

--- helpfiles/select_df.py
+++ helpfiles/select_df.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[86]:
+# In[19]:
 
 
 #aantal mappen terug naar DataProcessing #de dataset die je wil importeren 
@@ -11,15 +11,13 @@ def select_df(folders_up, dataset):
     import pandas as pd
     import numpy as np
     
-    print(os.getcwd())
-    
     if dataset == 'foodprices':
         for i in range(folders_up):
             os.chdir("../")
         os.chdir("datasets/preprocessed datasets/Food prices")
         raw_data = pd.read_csv('Food_prizes_Africa_processed.csv')
         df = pd.DataFrame(raw_data)
-        print('imported dataset as: df_foodprices')   
+        print('Succesfully imported foodprices!')   
         
     elif dataset == 'migration_movements':
         for i in range(folders_up):
@@ -27,7 +25,7 @@ def select_df(folders_up, dataset):
         os.chdir("datasets/preprocessed datasets/Migration movements")
         raw_data = pd.read_csv('data_africa.csv')
         df = pd.DataFrame(raw_data)
-        print('imported dataset as: df_migration_movements')    
+        print('Succesfully imported migration_movements!')    
                                     
     elif dataset == 'temperature_and_precipitation':
         for i in range(folders_up):
@@ -35,15 +33,15 @@ def select_df(folders_up, dataset):
         os.chdir("datasets/preprocessed datasets/Temperature and precipitation")
         raw_data = pd.read_csv('tas_pr_1991_2015_AC.csv')
         df = pd.DataFrame(raw_data)
-        print('imported dataset as: df_temperature_and_precipitation')
+        print('succesfully imported temperature_and_precipitation!')
+        
     else:
         return('Error: -- {} -- is geen beschikbare dataset. Kies uit: foodprices, migration_movements of temperature_and_precipitation'.format(dataset))
     
     if folders_up < 3:    
         for i in range(3 - folders_up ):
             os.chdir("../")
-
-    print(os.getcwd())
     return df
-select_df(2, 'hoi')
 
+# werking
+# select_df(1, 'migration_movements')
